9bad8835d1beda3a44730f052c8800fc4a5b2ce6
Nikhil Kumar <Digital@netpuppys.com>
2026-08-20 12:36:24 +0530

    fix

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className="bg-white text-black antialiased">
         <main>{children}</main>
       </body>
     </html>
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,9 @@
 import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
 import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
-
 import config from '@/payload.config'
 import './styles.css'
+import Link from 'next/link'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -13,47 +11,15 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/3.x/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/3.x/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
+    <div className="mx-auto flex h-screen max-w-2xl flex-col items-center justify-center gap-8 overflow-hidden p-8 sm:p-11">
+      {!user && <Image src="/tulas-logo.png" alt="Tulas logo" width={400} height={500} priority />}
+      <Link
+        href={payloadConfig.routes.admin}
+        className="rounded-md border border-black bg-black px-4 py-2 text-white transition-opacity hover:opacity-80"
+      >
+        Login
+      </Link>
     </div>
   )
 }
